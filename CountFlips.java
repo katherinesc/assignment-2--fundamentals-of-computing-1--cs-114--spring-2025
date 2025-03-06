5d1914d8090a4c7e128935b8b3128794a83125cd
katherinesc <kcorrea10506@gmail.com>
2025-03-05 19:49:50 -0500

    Coin flipped 100 times

--- CountFlips.java
+++ CountFlips.java
@@ -1,10 +1,26 @@
 public class CountFlips {
   public static void main(String[] args) {
+    int MAX = 100;
+    int heads = 0;
+    int tails = 0;
+    int totalCoinflips = 0;
+
     Coin myCoin = new Coin();
 
     myCoin.flip();
 
-    System.out.println(myCoin);
-
+    while (totalCoinflips < MAX){
+      if (myCoin.isHeads()) {
+        heads ++;
+        totalCoinflips ++;
+        myCoin.flip();
+      } else {
+        tails ++;
+        totalCoinflips ++;
+        myCoin.flip();
+    }
+    }
+    System.out.println("Heads: " + heads);
+    System.out.println("Tails: " + tails);
   }
-}
+  }
